1788fcd19bb397136b77c70d0953a7170b5452d1
Hanh Doan <haahdoan@gmail.com>
2018-07-22 22:56:12 +0700

    Feedback

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,48 +1,27 @@
 class CartsController < ApplicationController
   before_action :check_login
-<<<<<<< HEAD
   before_action :find_cart, only: [:update, :destroy]
 
   def show
     @cart = current_user.carts
-=======
-  before_action :find_cart_user, only: [:show, :create]
-
-  def show
->>>>>>> Add Update Delete Purchased cart
     @total_price = Cart.total_price @cart
   end
 
   def create
-<<<<<<< HEAD
     cart = current_user.carts.find_by product_id: params[:product_id]
-=======
-    cart = @cart.find_by product_id: params[:product_id]
->>>>>>> Add Update Delete Purchased cart
     if cart.present?
       quantity = cart.quantity + params[:quantity].to_i
       cart.update_column :quantity, quantity
     else
-<<<<<<< HEAD
       cart = current_user.carts.build cart_params
       cart.save
-=======
-      Cart.create! quantity: params[:quantity],
-        product_id: params[:product_id], user_id: current_user.id
->>>>>>> Add Update Delete Purchased cart
     end
     Product.update_quantity 0, params[:quantity].to_i, params[:product_id]
     redirect_to_back
   end
 
-<<<<<<< HEAD
   def update
     return unless @cart
-=======
-  def update 
-    @cart = Cart.find_by id: params[:cart_id]
-    return unless current_user = @cart.user
->>>>>>> Add Update Delete Purchased cart
     Product.update_quantity @cart.quantity, params[:quantity].to_i,
       @cart.product.id
     @cart.update_column :quantity, params[:quantity].to_i
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root "products#index"
   resources :categories, only: %i(index show)
   resources :products, only: %i(index show)
+  resources :orders, only: %i(show create)
+  resources :carts, only: %i(show create update destroy)
   resources :orders, only: %i(create)
   resources :carts, only: %i(show create update destroy)
+  resources :feedbacks, only: %i(new create)
   namespace :account do
     resources :sessions, only: %i(new create destroy)
     resources :users, only: %i(new create show edit update)
